c6ec9d4e643432ba667cc2e32530c69cced88f7b
Gatongone <gatongone@gmail.com>
2025-01-27 18:33:19 +0800

    feat: add theme config.

---
+++ lua/theme/scheme/popnlock.lua
@@ -0,0 +1,40 @@
+local M = {}
+M.color = 
+{
+    dark             = "#0d0e0f",
+    foreground       = "#ebdbb2",
+    background       = "#1c1c2d",
+    background_dark  = "#141421",
+    background_light = "#141421",
+    medium_gray      = "#928374",
+    comment          = "#928374",
+    gray             = "#DEDEDE",
+    soft_yellow      = "#eebd35",
+    soft_green       = "#98971a",
+    bright_yellow    = "#fabd2f",
+    orange           = "#d65d0e",
+    red              = "#fb4934",
+    error_red        = "#cc241d",
+    magenta          = "#b16286",
+    pink             = "#d4879c",
+    light_blue       = "#5394ec",
+    dark_gray        = "#83a598",
+    blue_gray        = "#299999",
+    forest_green     = "#689d6a",
+    clean_green      = "#8ec07c",
+    milk             = "#e7d7ad",
+    none             = "NONE",
+}
+M.term =
+{
+  "#282828", "#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a", "#ebdbb2",
+  "#928374", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#a89984",
+}
+M.diff =
+{
+  add    = "#28471e",
+  change = "#383d70",
+  delete = "#86033e",
+  text   = "#314753",
+}
+return M
