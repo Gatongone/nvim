9448ae493d30f8c53de6da76f4ad4dee18cd9667
Gatongone <gatongone@gmail.com>
2025-02-06 07:10:46 +0800

    chore: adjust noice appearance.

--- lua/plug/lazy/noice.lua
+++ lua/plug/lazy/noice.lua
@@ -1,28 +1,50 @@
-local colors = require("theme.scheme."..nvim.setting.appearance.theme).color
+local colors = require("theme.scheme." .. nvim.setting.appearance.theme).color
+
+local lsp_opts =
+{
+    lsp =
+    {
+        progress =
+        {
+            enabled = true,
+            format  =
+            {
+                {
+                    "",
+                    key = "progress.percentage",
+                    contents = { { "{data.progress.message} " } },
+                },
+                "({data.progress.percentage}%) ",
+                { "{spinner} ",              hl_group = "NoiceLspProgressSpinner" },
+                { "{data.progress.title} ",  hl_group = "NoiceLspProgressTitle" },
+                { "{data.progress.client} ", hl_group = "NoiceLspProgressClient" },
+            }
+        }
+    },
+    cmdline =
+    {
+        format = { cmdline = { title = ' Command ' } },
+    },
+    presets =
+    {
+        lsp_doc_border = true
+    },
+    hover =
+    {
+        enabled = true,
+        silent  = false,
+        view    = nil,
+        opts    = {},
+    },
+}
 
 return
 {
     "folke/noice.nvim",
     event = "VeryLazy",
     config = function()
-        require('noice').setup(
-        {
-            presets =
-            {
-		        lsp_doc_border = true
-	        },
-            hover =
-            {
-                enabled = true,
-                silent = false,
-                view = nil,
-                opts = {},
-            },
-        })
-        require('notify').setup(
-        {
-            background_colour = colors.background,
-        })
+        require('noice').setup(lsp_opts)
+        require('notify').setup({ background_colour = colors.background })
     end,
     dependencies =
     {
