1ef6e0bf91140f593608eec4b546ec8a87042954
Gatongone <gatongone@gmail.com>
2025-02-21 01:46:34 +0800

    chore: use nvim.keymap.editor.close instead.

--- lua/core/keymap/tab.lua
+++ lua/core/keymap/tab.lua
@@ -2,7 +2,5 @@ local tab = nvim.keymap.tab
 
 nmap(tab.prev_tab,      ':-tabnext<CR>')
 nmap(tab.next_tab,      ':+tabnext<CR>')
-nmap(tab.close_tab,     ':tabclose<CR>')
 vmap(tab.prev_tab,      ':-tabnext<CR>')
 vmap(tab.next_tab,      ':+tabnext<CR>')
-vmap(tab.close_tab,     ':tabclose<CR>')
